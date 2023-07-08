40b1095e4133eeb4db6832a1705b61983fb3405f
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-08 02:45:26 -0400

    Update views.py

--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,39 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
+
+@api_view(["GET", "POST"])
+def songs_list(request):
+
+    if request.method == "GET":
+        products = Song.objects.all()
+        serializer = SongSerializer(songs, many =True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = SongSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+    
+
+@api_view(["GET", "PUT", "DELETE"])
+def songs_detail(request, pk):
+    song = get_object_or_404(Song, pk =pk)
+    if request.method == "GET":
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = SongSerializer(song, data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        song.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
 
 # Create your views here.
