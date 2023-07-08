e225973ecb3c58338c74221f2b00ec9e3946b130
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-08 02:52:33 -0400

    Update urls.py

--- songs_project/urls.py
+++ songs_project/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.songs_list),
-    path('<int:pk>/', views.songs_detail),
-    
+    path('admin/', admin.site.urls),
+    path('api/songs/', include ('songs.urls'))
 ]
