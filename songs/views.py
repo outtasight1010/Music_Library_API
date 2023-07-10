880d263a50dca45c8860170bf0f2c2c9dbef9453
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-09 22:32:41 -0400

    Update views.py

--- songs/views.py
+++ songs/views.py
@@ -21,19 +21,19 @@ def songs_list(request):
         return Response(serializer.data, status = status.HTTP_201_CREATED)
     
 
-#@api_view(["GET", "PUT", "DELETE"])
-#def songs_detail(request, pk):
-    #song = get_object_or_404(Song, pk =pk)
-    #if request.method == "GET":
-        #serializer = SongSerializer(song)
-        #return Response(serializer.data)
-    #elif request.method == "PUT":
-        #serializer = SongSerializer(song, data= request.data)
-        #serializer.is_valid(raise_exception=True)
-        #serializer.save()
-        #return Response(serializer.data)
-    #elif request.method == "DELETE":
-        #song.delete()
-        #return Response(status = status.HTTP_204_NO_CONTENT)
+@api_view(["GET", "PUT", "DELETE"])
+def songs_detail(request, pk):
+    song = get_object_or_404(Song, pk =pk)
+    if request.method == "GET":
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = SongSerializer(song, data= request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        song.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
 
 # Create your views here.
