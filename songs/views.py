3479dac18a3cbe06d211ea382eb0142167d7f540
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-09 20:45:12 -0400

    made migrations to songs app

--- songs/views.py
+++ songs/views.py
@@ -3,22 +3,22 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-#from .serializers import SongSerializer
-#from .models import Song
+from .serializers import SongSerializer
+from .models import Song
 
-#@api_view(["GET", "POST"])
-#def songs_list(request):
+@api_view(["GET", "POST"])
+def songs_list(request):
 
-    #if request.method == "GET":
-        #products = Song.objects.all()
-        #serializer = SongSerializer(songs, many =True)
-        #return Response(serializer.data)
+    if request.method == "GET":
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many =True)
+        return Response(serializer.data)
     
-    #elif request.method == "POST":
-        #serializer = SongSerializer(data = request.data)
-        #serializer.is_valid(raise_exception=True)
-        #serializer.save()
-        #return Response(serializer.data, status = status.HTTP_201_CREATED)
+    elif request.method == "POST":
+        serializer = SongSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
     
 
 #@api_view(["GET", "PUT", "DELETE"])
