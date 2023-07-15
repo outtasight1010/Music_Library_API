36a16fc9d43bc29396b3de23885854ca095d9f5c
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-15 03:45:39 -0400

    update SearchBar css

--- musica/src/Components/SearchBar/SearchBar.jsx
+++ musica/src/Components/SearchBar/SearchBar.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import './SearchBar.css';
 
 const SearchBar = () => {
     return(
-    <div>
+    <div className='search-bar'>
         <input type = 'text' placeholder = 'Search...'/>
         <button>Search</button>
 
