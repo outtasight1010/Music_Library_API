7e320efc951962ff963756c793fa38c568df8fa2
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-26 05:05:24 -0400

    update NavBar css

--- musica/src/Components/NavBar/NavBar.jsx
+++ musica/src/Components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import  './NavBar.css';
 
+
 const NavigationBar = () => {
     return(
         <nav className = 'nav'>
         <h1>
             MUSIC HIT LIST
+            
         </h1>
         </nav>
     );
